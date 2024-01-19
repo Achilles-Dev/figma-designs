29b8b3934419f5979f59e693d853650fdd648fe4
Achilles-Dev <solohag92@gmail.com>
2024-01-19 16:32:11 +0000

    Add Payment method modal

--- src/components/payoutsComponents/MyModal.tsx
+++ src/components/payoutsComponents/MyModal.tsx
@@ -1,80 +1,10 @@
-import React from 'react'
-import CloseIcon from '../../assets/cross.svg'
-import InfoIcon from '../../assets/Info.svg'
+import React, { ReactNode } from 'react'
 
-interface ModalProps {
-  isOpen: boolean
-  onClose: () => void
-}
-
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) {
-    return null
-  }
-
+const Modal = ({ children }: { children: ReactNode }) => {
   return (
     <div className='fixed inset-0 bg-[#1A181E] bg-opacity-60 flex items-center justify-center'>
-      <div className='bg-white p-[24px] flex flex-col gap-[24px] rounded-lg w-96 text-center box-shadow-[0_2px_6px_0px_#1A181E0A] w-[564px]'>
-        <div className='flex flex-col gap-[16px]'>
-          <div className='flex flex-col gap-[4px]'>
-            <div className='flex justify-between'>
-              <p className='text-[18px] leading-[26px] font-medium text-[#1A181E]'>
-                Set up Cashfree Payments
-              </p>
-              <button>
-                <img
-                  onClick={onClose}
-                  className='h-[24px] w-[24px]'
-                  src={CloseIcon}
-                  alt='close'
-                />
-              </button>
-            </div>
-            <p className='text-[14px] leading-[20px] text-[#4D4D4D] text-start'>
-              Please enter your Cashfree API keys. You can find your API keys on
-              <br />
-              <span className='text-[#146EB4] font-medium underline'>
-                Cashfree API Key Dashboard
-              </span>
-            </p>
-          </div>
-          <div className='account flex justify-center gap-[8px] px-[16px] py-[8px] rounded'>
-            <div className='flex gap-[8px]'>
-              <img onClick={onClose} src={InfoIcon} alt='Info' />
-              <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
-                Don’t have an Cashfree account?{' '}
-                <button className='font-medium underline'>Create now</button>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-col gap-[32px]'>
-          <div className='flex flex-col gap-[24px]'>
-            <div className='flex flex-col items-start gap-[8px]'>
-              <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
-                API ID <span className='text-[#E50B20]'>*</span>
-              </p>
-              <input
-                className='border-[1px] w-full rounded border-[#D9D9D9] py-[12px] px-[16px] placeholder:text-[16px] placeholder:leading-[24px] placeholder:text-[#999999]'
-                placeholder='Enter merchant id'
-              />
-            </div>
-            <div className='flex flex-col items-start gap-[8px]'>
-              <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
-                Secrey Key <span className='text-[#E50B20]'>*</span>
-              </p>
-              <input
-                className='border-[1px] w-full rounded border-[#D9D9D9] py-[12px] px-[16px] placeholder:text-[16px] placeholder:leading-[24px] placeholder:text-[#999999]'
-                placeholder='Enter merchant id'
-              />
-            </div>
-          </div>
-        </div>
-        <div className='flex justify-center'>
-          <button className='px-[24px] py-[8px] bg-[#146EB4] opacity-40 rounded text-white'>
-            Continue
-          </button>
-        </div>
+      <div className='bg-white flex flex-col gap-[24px] rounded-lg w-96 text-center box-shadow-[0_2px_6px_0px_#1A181E0A] w-[564px]'>
+        {children}
       </div>
     </div>
   )
--- src/components/payoutsComponents/Payments1.tsx
+++ src/components/payoutsComponents/Payments1.tsx
@@ -3,6 +3,18 @@ import CashfreeImage from '../../assets/Cashfree.png'
 import PhonePeImage from '../../assets/PhonePe.png'
 import PaytmImage from '../../assets/Paytm.png'
 import CODImage from '../../assets/COD.png'
+import CloseIcon from '../../assets/cross.svg'
+import InfoIcon from '../../assets/Info.svg'
+import VisaIcon from '../../assets/Visa.svg'
+import MastercardIcon from '../../assets/Mastercard.svg'
+import MaestroIcon from '../../assets/Maestro.svg'
+import RupayIcon from '../../assets/Rupay.svg'
+import AMEXIcon from '../../assets/Amex.svg'
+import DinerClubIcon from '../../assets/Diner_Club.svg'
+import PaytmIcon from '../../assets/PaytmPM.svg'
+import AirtelMoneyIcon from '../../assets/Airtel.svg'
+import AmazonPayIcon from '../../assets/Amazon.svg'
+
 import { useState } from 'react'
 import Modal from './MyModal'
 
@@ -35,17 +47,70 @@ const providers = [
   },
 ]
 
+const paymentMethods = [
+  {
+    name: 'Visa',
+    image: VisaIcon,
+  },
+  {
+    name: 'Mastercard',
+    image: MastercardIcon,
+  },
+  {
+    name: 'Maestro',
+    image: MaestroIcon,
+  },
+  {
+    name: 'Rupay',
+    image: RupayIcon,
+  },
+  {
+    name: 'American Express',
+    image: AMEXIcon,
+  },
+  {
+    name: 'Diner Club',
+    image: DinerClubIcon,
+  },
+  {
+    name: 'Paytm',
+    image: PaytmIcon,
+  },
+  {
+    name: 'Airtel Money',
+    image: AirtelMoneyIcon,
+  },
+  {
+    name: 'Amazon Pay',
+    image: AmazonPayIcon,
+  },
+]
+
+const methodSelect = () => {
+  let items = {} as any
+  paymentMethods.forEach((pmt) => {
+    let name = pmt.name
+    items[name] = false
+  })
+  return items
+}
+
 const Payments1 = () => {
   const [isCODChecked, setIsCODChecked] = useState(false)
   const [isMPMClick, setIsMPMClick] = useState(false)
-  const [isModalOpen, setModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isPmtModalOpen, setIsPmtModalOpen] = useState(false)
+  const [pmtMtdSelect, setPmtMtdSelect] = useState<any>(methodSelect())
+  const [setupModalHeading, setSetupModalHeading] = useState<string>('')
 
-  const openModal = () => {
-    setModalOpen(true)
-  }
+  console.log(pmtMtdSelect)
 
   const closeModal = () => {
-    setModalOpen(false)
+    if (isPmtModalOpen) {
+      setIsPmtModalOpen(false)
+    } else {
+      setIsModalOpen(false)
+    }
   }
 
   const handleCODClick = () => {
@@ -57,7 +122,22 @@ const Payments1 = () => {
   }
 
   const handleSetup = (name: string) => {
-    console.log(name)
+    setSetupModalHeading(name)
+    setIsModalOpen(true)
+  }
+
+  const handleContinue = () => {
+    setIsPmtModalOpen(true)
+  }
+
+  const handlePaymentMethodSelect = (name: string) => {
+    setPmtMtdSelect((prev: any) => {
+      let value = prev[name]
+      return {
+        ...prev,
+        [name]: !value,
+      }
+    })
   }
 
   return (
@@ -155,16 +235,140 @@ const Payments1 = () => {
           </button>
         </div>
       </div>
+      <div className='min-h-screen flex items-center justify-center'>
+        {!isModalOpen ? (
+          ''
+        ) : (
+          <Modal>
+            <div className='flex flex-col p-[24px] gap-[24px]'>
+              <div className='flex flex-col gap-[16px]'>
+                <div className='flex flex-col gap-[4px]'>
+                  <div className='flex justify-between'>
+                    <p className='text-[18px] leading-[26px] font-medium text-[#1A181E]'>
+                      Set up {setupModalHeading}
+                    </p>
+                    <button>
+                      <img
+                        onClick={closeModal}
+                        className='h-[24px] w-[24px]'
+                        src={CloseIcon}
+                        alt='close'
+                      />
+                    </button>
+                  </div>
+                  <p className='text-[14px] leading-[20px] text-[#4D4D4D] text-start'>
+                    Please enter your Cashfree API keys. You can find your API
+                    keys on
+                    <br />
+                    <span className='text-[#146EB4] font-medium underline'>
+                      Cashfree API Key Dashboard
+                    </span>
+                  </p>
+                </div>
+                <div className='account flex justify-center gap-[8px] px-[16px] py-[8px] rounded'>
+                  <div className='flex gap-[8px]'>
+                    <img onClick={closeModal} src={InfoIcon} alt='Info' />
+                    <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
+                      Don’t have an Cashfree account?{' '}
+                      <button className='font-medium underline'>
+                        Create now
+                      </button>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col gap-[32px]'>
+                <div className='flex flex-col gap-[24px]'>
+                  <div className='flex flex-col items-start gap-[8px]'>
+                    <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
+                      API ID <span className='text-[#E50B20]'>*</span>
+                    </p>
+                    <input
+                      className='border-[1px] w-full rounded border-[#D9D9D9] py-[12px] px-[16px] placeholder:text-[16px] placeholder:leading-[24px] placeholder:text-[#999999]'
+                      placeholder='Enter merchant id'
+                    />
+                  </div>
+                  <div className='flex flex-col items-start gap-[8px]'>
+                    <p className='text-[14px] leading-[20px] text-[#4D4D4D]'>
+                      Secrey Key <span className='text-[#E50B20]'>*</span>
+                    </p>
+                    <input
+                      className='border-[1px] w-full rounded border-[#D9D9D9] py-[12px] px-[16px] placeholder:text-[16px] placeholder:leading-[24px] placeholder:text-[#999999]'
+                      placeholder='Enter merchant id'
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='flex justify-center'>
+                <button
+                  onClick={handleContinue}
+                  className='px-[24px] py-[8px] bg-[#146EB4] opacity-40 rounded text-white'
+                >
+                  Continue
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
+      </div>
 
       <div className='min-h-screen flex items-center justify-center'>
-        <button
-          className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded'
-          onClick={openModal}
-        >
-          Open Modal
-        </button>
+        {!isPmtModalOpen ? (
+          ''
+        ) : (
+          <Modal>
+            <div className='flex justify-between p-[24px]'>
+              <p>Choose payment methods</p>
+              <button>
+                <img
+                  onClick={closeModal}
+                  className='h-[24px] w-[24px]'
+                  src={CloseIcon}
+                  alt='close'
+                />
+              </button>
+            </div>
+            <div>
+              <div className='flex gap-[24px] px-[24px] h-[58vh] overflow-y-auto '>
+                <div className='flex flex-col gap-[16px] w-full'>
+                  {paymentMethods.map((payMeth, index) => (
+                    <div
+                      className='flex gap-[24px] justify-between items-center rounded'
+                      key={index}
+                    >
+                      <div className='flex gap-[16px] items-center'>
+                        <img src={payMeth.image} alt={payMeth.name} />
+                        <p className='text-[16px] leading-[24px] text-[#1A181E] font-medium'>
+                          {payMeth.name}
+                        </p>
+                      </div>
+                      <div
+                        className='w-[36px] h-[24px] relative inline-block'
+                        onClick={() => handlePaymentMethodSelect(payMeth.name)}
+                      >
+                        <input
+                          className='opacity-0 w-[0px] h-[0px]'
+                          type='checkbox'
+                          checked={pmtMtdSelect[payMeth.name]}
+                        />
+                        <span className='slider round'></span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className='flex flex-col items-center gap-[8px] border-t-[1px] border-[#E6E6E6] py-[16px]'>
+                <button className='flex px-[24px] py-[8px] rounded bg-[#146EB4] text-white text-[16px] leading-[24px] font-medium'>
+                  Finish setup
+                </button>
 
-        <Modal isOpen={isModalOpen} onClose={closeModal} />
+                <button className='underline text-[16px] leading-[24px] font-medium text-[#4D4D4D]'>
+                  Edit Cashfree details
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     </main>
   )
